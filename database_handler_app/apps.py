a962a6ef205d2f04ac039f31e82232b7f146d4ca
sebajou <sebajou@gmail.com>
2020-10-16 11:03:20 +0200

    All files

--- database_handler_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatabaseHandlerAppConfig(AppConfig):
-    name = 'database_handler_app'
--- request_api_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequestApiAppConfig(AppConfig):
-    name = 'request_api_app'
