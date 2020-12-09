1e5c5b1305f43de8544a7f66c8b4833e93b7c30f
sebajou <sebajou@gmail.com>
2020-12-09 02:06:08 +0100

    Update setting.py for static file for Heroku deployment5

--- PurBeurre_project/PurBeurre_project/settings.py
+++ PurBeurre_project/PurBeurre_project/settings.py
@@ -143,9 +143,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
