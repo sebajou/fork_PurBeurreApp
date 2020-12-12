56b1c3e0e8d5324d7db4b0a0c672befcf37fe493
sebajou <sebajou@gmail.com>
2020-12-12 23:47:48 +0100

    Improve add tables in admin.py

--- PurBeurre_project/database_handler_app/admin.py
+++ PurBeurre_project/database_handler_app/admin.py
@@ -3,5 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 from .models import MyUsers, Allergen, Diet, FoodList, Favorites
 
 
-admin.site.register(Allergen, Diet, FoodList, Favorites)
+admin.site.register(Allergen)
+admin.site.register(Diet)
+admin.site.register(FoodList)
+admin.site.register(Favorites)
 admin.site.register(MyUsers, UserAdmin)
