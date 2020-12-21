d98bf3e5eac5148e5e94611ea59026aff8a71bf2
sebajou <sebajou@gmail.com>
2020-12-21 16:11:36 +0100

    Fix little error in manage.py

--- PurBeurre_project/PurBeurre_project/settings.py
+++ PurBeurre_project/PurBeurre_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'stormy-earth-30718.herokuapp.com']
 
