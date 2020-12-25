62515243a2581f2ce85ca9c93582c0b86e9a342c
sebajou <sebajou@gmail.com>
2020-12-25 21:08:06 +0100

    Modify Raven setting for Sentry

--- PurBeurre_project/PurBeurre_project/settings.py
+++ PurBeurre_project/PurBeurre_project/settings.py
@@ -184,58 +184,58 @@ else:
 
 django_heroku.settings(locals())
 
-if PRODUCTION:
-    RAVEN_CONFIG = {
-        'dsn': 'https://65e24de4a51448509d03458c623778b6@o496144.ingest.sentry.io/5570010',
-        # If you are using git, you can also automatically configure the
-        # release based on the git info.
-        'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
-    }
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': True,
-        'root': {
-            'level': 'INFO',  # WARNING by default. Change this to capture more than warnings.
-            'handlers': ['sentry'],
-        },
-        'formatters': {
-            'verbose': {
-                'format': '%(levelname)s %(asctime)s %(module)s '
-                          '%(process)d %(thread)d %(message)s'
-            },
-        },
-        'handlers': {
-            'sentry': {
-                'level': 'INFO',  # To capture more than ERROR, change to WARNING, INFO, etc.
-                'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-                'tags': {'custom-tag': 'x'},
-            },
-            'console': {
-                'level': 'DEBUG',
-                'class': 'logging.StreamHandler',
-                'formatter': 'verbose'
-            }
-        },
-        'loggers': {
-            'django.db.backends': {
-                'level': 'ERROR',
-                'handlers': ['console'],
-                'propagate': False,
-            },
-            'raven': {
-                'level': 'DEBUG',
-                'handlers': ['console'],
-                'propagate': False,
-            },
-            'sentry.errors': {
-                'level': 'DEBUG',
-                'handlers': ['console'],
-                'propagate': False,
-            },
-        },
-    }
-else:
-    pass
+# if PRODUCTION:
+#     RAVEN_CONFIG = {
+#         'dsn': 'https://65e24de4a51448509d03458c623778b6@o496144.ingest.sentry.io/5570010',
+#         # If you are using git, you can also automatically configure the
+#         # release based on the git info.
+#         'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+#     }
+#     LOGGING = {
+#         'version': 1,
+#         'disable_existing_loggers': True,
+#         'root': {
+#             'level': 'INFO',  # WARNING by default. Change this to capture more than warnings.
+#             'handlers': ['sentry'],
+#         },
+#         'formatters': {
+#             'verbose': {
+#                 'format': '%(levelname)s %(asctime)s %(module)s '
+#                           '%(process)d %(thread)d %(message)s'
+#             },
+#         },
+#         'handlers': {
+#             'sentry': {
+#                 'level': 'INFO',  # To capture more than ERROR, change to WARNING, INFO, etc.
+#                 'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+#                 'tags': {'custom-tag': 'x'},
+#             },
+#             'console': {
+#                 'level': 'DEBUG',
+#                 'class': 'logging.StreamHandler',
+#                 'formatter': 'verbose'
+#             }
+#         },
+#         'loggers': {
+#             'django.db.backends': {
+#                 'level': 'ERROR',
+#                 'handlers': ['console'],
+#                 'propagate': False,
+#             },
+#             'raven': {
+#                 'level': 'DEBUG',
+#                 'handlers': ['console'],
+#                 'propagate': False,
+#             },
+#             'sentry.errors': {
+#                 'level': 'DEBUG',
+#                 'handlers': ['console'],
+#                 'propagate': False,
+#             },
+#         },
+#     }
+# else:
+#     pass
 
 if PRODUCTION:
     sentry_sdk.init(
